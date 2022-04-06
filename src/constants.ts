b3b9a4c82f91ba699c46a30114576e2d1266a8cb
Kreedzt <zhaozisong1@live.com>
2022-04-06 22:36:30 +0800

    :release: Kreedzt - 0.1.4

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,5 @@ export const QUERY_TIME_STORAGE_KEY = 'query-time';
 export const QUERY_CACHE_STORAGE_KEY = 'cache';
 export const BUTTON_EXPIRED_STORAGE_KEY = 'button-expired';
 export const USER_ID_NOT_FOUND_MSG = "找不到用户信息";
-export const VERSION = "0.1.3";
+export const VERSION = "0.1.4";
 export const API_PREFIX = "/api";
