56d11166ad0b92e0e0c96e86e5d88348b1732d3b
Kreedzt <zhaozisong1@live.com>
2022-09-27 19:33:27 +0800

    :construction: Kreedzt - replace RefreshButton className

--- src/components/refreshButton/RefreshButton.tsx
+++ src/components/refreshButton/RefreshButton.tsx
@@ -21,10 +21,21 @@ const RefreshButton: FC<RefreshButtonProps> = ({ loading, onRefresh }) => {
     await CacheService.refreshButtonExpireTime();
   }, []);
 
+  // return (
+  //   <Button danger loading={loading} onClick={refresh}>
+  //     强制重新获取(请求时间稍长, 限制1分钟请求一次)
+  //   </Button>
+  // );
+
   return (
-    <Button danger loading={loading} onClick={refresh}>
+    <button
+      type="button"
+      className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm disabled:bg-red-200"
+      disabled={loading}
+      onClick={refresh}
+    >
       强制重新获取(请求时间稍长, 限制1分钟请求一次)
-    </Button>
+    </button>
   );
 };
 
